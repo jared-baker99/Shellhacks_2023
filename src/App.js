f56b6b9ae09960b6a5768067c07880e597ce0316
jared-baker99 <j.baker.99@knights.ucf.edu>
2023-09-16 02:48:58 -0400

    test changes

--- src/App.js
+++ src/App.js
@@ -7,16 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Learn all about insurance.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Click the button bellow to start your expierence</p>
       </header>
     </div>
   );
