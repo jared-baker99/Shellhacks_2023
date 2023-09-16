cd76868a73f6bf59e2c0242d89cab588b80f2e63
aaresecheona <aj.echeona@gmail.com>
2023-09-16 13:50:03 -0400

    Stuff

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import MainPage from './Pages/MainPage';
 import HomePage from './Pages/HomePage';
+import LifeInsurance from './Pages/LifeInsurace';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" index element={<MainPage />} />
         <Route path="/home" index element={<HomePage />} />
+        <Route path="/life" index element={<LifeInsurance />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/Pages/LifeInsurace.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const LifeInsurance = () =>
+{
+
+    return(
+        <div>
+            title
+            <div>
+                <p>click this button to learn more about insurance</p>
+                
+            </div>
+        </div>
+    );
+};
+
+export default LifeInsurance;
