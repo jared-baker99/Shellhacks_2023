b1250ebd111f7ad543300617251cd665a0971fb4
jared-baker99 <j.baker.99@knights.ucf.edu>
2023-09-16 08:00:17 -0400

    list of buttons from array input

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,13 +1,25 @@
 import React from 'react';
 
+import ButtonGroup from '../Components/ButtonGroup';
+
 const HomePage = () =>
 {
+    const printButtonLabel = (event) =>
+    {
+        alert(event.target.name);
+        //window.location.href = "/" +event.target.name;
+    }
+
     return(
         <div>
             title
             <div>
                 <p>Select an insurance company you want to learn about</p>
-                <button>State Farm</button>
+                <div>
+                    <ButtonGroup buttons={["One", "Two", "Three"]}
+                        doSomethingAfterClick={printButtonLabel}
+                    />
+                </div>
             </div>
         </div>
     );
