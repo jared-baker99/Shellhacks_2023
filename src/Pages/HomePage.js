783670d3edf05a980323827fb3fa6d8f34c210bd
natsumi <mya.ramsey@ulf.edu>
2023-09-16 20:15:56 -0400

    tring to fix this

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,31 +1,36 @@
 import React from 'react';
+import '../Styles/Styles.css'
 
-import ButtonGroup from '../Components/ButtonGroup';
 
 const HomePage = () =>
 {
-    const printButtonLabel = (event) =>
-    {
-        alert(event.target.name);
-        if (event.target.name === "Register"){
-            window.location.href = "/register";
-        }
-    }
-
     return(
-        <div>
-            title
-            <div>
-                <p>Select an insurance company you want to learn about</p>
-                <div className='buttons'>
-                    <ButtonGroup buttons={["Register", "Two", "Three"]}
-                        doSomethingAfterClick={printButtonLabel}
-                    />
+        <div className='UserRegister'>
+            <div className='Row'>
+                <div className='Box'>
+                    <h1 className='insurancetext'>StateFarm</h1>
+                    <div className='circle'>
+                        <h2 className='typetext'>Car</h2>
+                        <p className='moneytext'>240$</p>    
+                    </div>
+                </div>
+                <div className='Box'>
+                <div className='circle'>
+                        <h2 className='typetext'>Home</h2>
+                        <p className='moneytext'>120$</p>    
+                    </div>
+                </div>
+                <div className='Box'>
+                <div className='circle'>
+                        <h2 className='typetext'>Rentor</h2>
+                        <p className='moneytext'>80$</p>    
+                    </div>
                 </div>
             </div>
+
+            
         </div>
     );
-    
 };
 
 export default HomePage;
